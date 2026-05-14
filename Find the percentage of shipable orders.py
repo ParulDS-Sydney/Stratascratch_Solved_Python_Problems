6efeabbfb1408d9be7cbd4adf398acff178a4ae3
Parul Shukla <parul.shukla345@gmail.com>
2026-05-14 20:35:09 +0530

    Create Find the percentage of shipable orders.py

---
+++ Find the percentage of shipable orders.py
@@ -0,0 +1,6 @@
+import pandas as pd
+merged_df = pd.merge(orders, customers, on='id', how='left')
+shippable_count = merged_df['address'].notnull().sum()
+total_orders = len(orders)
+percentage_shippable = (shippable_count / total_orders) * 100
+print(f"Percentage of shippable orders: {percentage_shippable}%")
